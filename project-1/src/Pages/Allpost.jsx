44314cb49c03d2678c6855d3cf8bd0846803520a
rohit kumar <kumarrohit9337@gmail.com>
2024-06-18 10:30:50 +0530

    Done

--- project-1/src/Pages/Allpost.jsx
+++ project-1/src/Pages/Allpost.jsx
@@ -6,14 +6,25 @@ import appwriteService from '../../Appwrite/database';
 function Allpost(){
     const [posts , setposts] = useState([]);
     useEffect(() => {}, []);
-    appwriteService.getpost(() => {})
+    appwriteService.getpost([]).then((posts) => {
+        if(posts){
+            setposts(posts.documents);
+        }
+    })
     
 
     return(
-        <div className="py-8">
-            <Container>
-                <Postcard/>
-            </Container>
+        <div className="py-8 w-full">
+           <Container>
+            <div className="flex flex-wrap">
+                {posts.map(() => (
+                    <div key={posts.$id} className="p-2 w-1/4">
+                        <Postcard post={post}/>
+
+                    </div>
+                ))}
+            </div>
+           </Container>
         </div>
     )
 }
